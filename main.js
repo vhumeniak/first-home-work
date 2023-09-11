fbbdd60a4d9d7e3c78f2433bc8a7d65695f3b242
vhumeniak <144223972+vhumeniak@users.noreply.github.com>
2023-09-11 17:39:03 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let q = 1
+let w = NaN
+let e = "ukd"
+let t = true
+let y = "undefined"
+
+let about = {
+    name: "Vasyl",
+    surname: "Humeniak",
+    age: 18,
+    city: "Ivano-Frankivsk"
+}
+console.log(q, w, e, t , y)
+console.table(about)
